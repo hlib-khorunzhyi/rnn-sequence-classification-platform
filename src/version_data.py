ed9443439ab9666902258c03c142d6ea8897d766
Hlib Khorunzhyi <khorunzhyi519@gmail.com>
2026-05-05 09:10:18 +0200

    Initialized DVC

--- src/version_data.py
+++ src/version_data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from src.config_schemas.config_schema import Config
 from src.utils import get_logger
 from src.utils.config_utils import get_config
-from src.utils.data_utils import commit_to_dvc, initialize_dvc_storage, initilaze_dvc
+from src.utils.data_utils import make_new_data_version, initialize_dvc_storage, initilaze_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -11,7 +11,7 @@ def version_data(config: Config) -> None:
     initilaze_dvc()
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
 
-    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
+    make_new_data_version(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 
 if __name__ == "__main__":
